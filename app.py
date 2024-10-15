f3b9f3256ebe10c4fcf950be9dab93a0be272425
eswanepo1 <etienne.swanepoel@aciworldwide.com>
2024-10-16 01:38:32 +0200

    looks nice now

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
+import os
 
 app = Flask(__name__)
 
@@ -11,9 +12,9 @@ def favicon():
 @app.route('/')
 def index():
     webapps = [
-        {"name": "Weather Station", "url": "http://10.0.0.138:8001", "description": "Weather base station"},
-        {"name": "Task-UI", "url": "http://10.0.0.138:8003", "description": "Web front end for the task-api"},
-        {"name": "Webapp 3", "url": "http://localhost:5003", "description": "will see you soon Webapp3;)"},
+        {"name": "Weather Station", "url": "http://10.0.0.138:8001", "description": "Weather base station","favicon": "http://10.0.0.138:8001/static/favicon.ico"},
+        {"name": "Task-UI", "url": "http://10.0.0.138:8003", "description": "Web front end for the task-api","favicon": "http://10.0.0.138:8003/static/favicon.ico"},
+        {"name": "Webapp 3", "url": "http://localhost:5003", "description": "will see you soon Webapp3;)","favicon": "https://example.com/weather-favicon.ico"},
         # Add more webapps as needed
     ]
     return render_template('index.html', webapps=webapps)
